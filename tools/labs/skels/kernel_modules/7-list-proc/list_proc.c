dcd81b6a03344c8d3aa0d3a8a8414755853736f2
mirub <miru.b99@gmail.com>
2022-03-16 00:35:42 +0200

    Added solutions

--- tools/labs/skels/kernel_modules/7-list-proc/list_proc.c
+++ tools/labs/skels/kernel_modules/7-list-proc/list_proc.c
@@ -2,6 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 /* TODO: add missing headers */
+#include <linux/sched/signal.h> 
 
 MODULE_DESCRIPTION("List current processes");
 MODULE_AUTHOR("Kernel Hacker");
@@ -12,8 +13,12 @@ static int my_proc_init(void)
 	struct task_struct *p;
 
 	/* TODO: print current process pid and its name */
+	pr_info("Current process: pid = %d; comm = %s\n", current->pid, current->comm);
 
 	/* TODO: print the pid and name of all processes */
+	pr_info("\nProcess list:\n\n");                             
+    for_each_process(p)                                         
+    	pr_info("pid = %d; comm = %s\n", p->pid, p->comm);
 
 	return 0;
 }
@@ -21,6 +26,8 @@ static int my_proc_init(void)
 static void my_proc_exit(void)
 {
 	/* TODO: print current process pid and name */
+	pr_info("Current process: pid = %d; comm = %s\n", current->pid, current->comm);
+
 }
 
 module_init(my_proc_init);
